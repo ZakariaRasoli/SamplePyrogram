176078fc81a6e36c8a640079446706aa6223489d
Zakaria Rasoli <zakaria.rasoli@Gmail.com>
2022-05-02 01:45:28 +0430

    Update on.py

--- on.py
+++ on.py
@@ -1,7 +1,6 @@
 from pyrogram import Client
 import configparser
 from messageHandler import messageHandler
-import mysql.connector as mysql
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -9,16 +8,6 @@ config.read('config.ini')
 admins = []
 for i in config['admins']:
     admins.append(config['admins'][i])
-
-db = mysql.connect(
-    host=config['database']['host'],
-    database=config['database']['database'],
-    user=config['database']['user'],
-    password=config['database']['password'],
-    use_unicode=True,
-    charset='utf8mb4',
-    connection_timeout=86400
-)
 
 
 app = Client(
