5591470b0f1692cc120a93cf1f00def5beb0cb8e
Zakaria Rasoli <zakaria.rasoli@Gmail.com>
2022-05-03 15:26:32 +0430

    Update messageHandler.py

--- messageHandler.py
+++ messageHandler.py
@@ -1,5 +1,6 @@
 from pyrogram.handlers import *
-from modules.sendInfo import *
+
+
 def messageHandler(app, admins, db):
     async def dump(client, message):
         await app.send_message(admins[1], message)
