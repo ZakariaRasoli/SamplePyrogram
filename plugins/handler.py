220cf6c96dad900f3a2a4a2ff4d3030977364ea8
Zakaria Rasoli <zakaria.rasoli@Gmail.com>
2022-05-06 10:22:40 +0430

    Update handler.py

--- plugins/handler.py
+++ plugins/handler.py
@@ -1,19 +1,17 @@
-import os, psutil
 from pyrogram.types import Message
 from pyrogram import Client, filters, errors, emoji
 from pyrogram.raw import functions
-
-from plugins.lib.fun import Fun
 
 from plugins.lib.globals import Admins
 
 prefixes = ["/", '!', '.', '#', '']
 fun_commands = ['id']
-admin_commands = ['usage']
+admin_commands = ['usage', 'ping', 'eval']
 admins = Admins.admins()
 
 @Client.on_message(filters.command(commands=fun_commands, prefixes=prefixes))
 async def fun(client, message: Message):
+    from plugins.lib.fun import Fun
     command = message.command[0]
     reply_to_message = message.reply_to_message
     if command == 'id':
@@ -24,19 +22,25 @@ async def fun(client, message: Message):
             from_id = message.text.split(" ")[1] if len(message.text) > 4 else message.from_user.id
             await Fun(client, admins).idCommand(message, from_id)
 
+
 @Client.on_message(filters.user(users=admins) & filters.command(commands=admin_commands, prefixes=prefixes))
 async def admin(client, message: Message):
+    from plugins.lib.admin import Admin
     command = message.command[0]
     if command == 'usage':
-        process = psutil.Process(os.getpid())
-        memory = get_size(process.memory_info().rss)
-        em = emoji.CARD_INDEX_DIVIDERS
-        await message.reply(f"{em} **Memory usage**: `{memory}`" )
+        await Admin(admins).usageCommand(message)
+    elif command == 'ping':
+        await Admin(admins).pingCommand(message)
+    elif command == 'eval':
+        await Admin(admins).evalCommand(client, message)
 
-
-def get_size(bytes, suffix="B"):
-    factor = 1024
-    for unit in ["", "K", "M", "G", "T", "P"]:
-        if bytes < factor:
-            return f"{bytes:.2f}{unit}{suffix}"
-        bytes /= factor
+@Client.on_edited_message(filters.user(users=admins) & filters.command(commands=admin_commands, prefixes=prefixes))
+async def editAdmin(client: Client, message: Message):
+    from plugins.lib.admin import Admin
+    command = message.command[0]
+    if command == 'eval':
+        msgs = []
+        async for msg in client.get_discussion_replies(message.chat.id, message.id):
+            msgs.append(msg)
+        msg_edit = msgs[len(msgs)-1]
+        await Admin(admins).evalCommand(client, message, msg_edit)
