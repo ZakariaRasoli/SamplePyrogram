72e07b411c9e4fdb128ace5f97f3bfa8967ebe79
Zakaria Rasoli <zakaria.rasoli@Gmail.com>
2022-06-04 00:21:31 +0430

    Update globals.py

--- lib/globals.py
+++ lib/globals.py
@@ -20,12 +20,7 @@ class Global():
         plug_name = os.path.basename(file).split('.', 1)[0]
         return [db().select_with_plug_name(plug_name)[0][2]]
 
-    def get_size(self, bytes, suffix="B"):
-        factor = 1024
-        for unit in ["", "K", "M", "G", "T", "P"]:
-            if bytes < factor:
-                return f"{bytes:.2f}{unit}{suffix}"
-            bytes /= factor
+    
 
 
 
