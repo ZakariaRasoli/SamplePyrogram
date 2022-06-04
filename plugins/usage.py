315183f0e006118015213500935ac2110aca74c5
Zakaria Rasoli <zakaria.rasoli@Gmail.com>
2022-06-04 12:44:03 +0430

    Update usage.py

--- plugins/usage.py
+++ plugins/usage.py
@@ -2,6 +2,7 @@ import os, psutil
 from pyrogram.types import *
 from pyrogram import Client, filters, emoji
 from lib.globals import Global
+from lib.functions import get_size
 
 prefixes = Global().prefixes()
 admins = Global().admins()
@@ -10,7 +11,6 @@ command = Global().get_command(__file__)
 @Client.on_message(filters.user(users=admins) & filters.command(commands=command, prefixes=prefixes))
 async def usage(client: Client, message: Message):
     process = psutil.Process(os.getpid())
-    memory = Global().get_size(process.memory_info().rss)
+    memory = get_size(process.memory_info().rss)
     em = emoji.CARD_INDEX_DIVIDERS
     await message.reply(f"{em} **Memory usage**: `{memory}`" )
-
