66a642c5c6574843b683114d99b104d830af2f6a
Zakaria Rasoli <zakaria.rasoli@Gmail.com>
2022-06-05 23:04:10 +0430

    Update db.py

--- lib/db.py
+++ lib/db.py
@@ -1,3 +1,4 @@
+import json
 import sqlite3
 
 class db:
@@ -6,8 +7,20 @@ class db:
         self.db = sqlite3.connect('database.db') 
         self.cur = self.db.cursor()
     
+    def query(self, query: str):
+        self.cur.execute(query)
+        self.db.commit()
+        return True
+
+    def select(self, query: str):
+        self.cur.execute(query)
+        result = []
+        for row in self.cur.fetchone():
+            result.append(row)
+        return result
+    
     def create(self, table: str, values: list):
-        ex = self.cur.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='%s' ''' %(table))
+        self.cur.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='%s' ''' %(table))
         if self.cur.fetchone()[0] == 1:
             self.drop(table)
         value = ''
@@ -18,62 +31,33 @@ class db:
                 value += "%s, " %(i)
         return self.db.execute('''CREATE TABLE %s (%s)''' %(table, value))
 
-    def insert(self, table: str, values: list):
-        value = '('
-        for i in values:
-            if i == values[-1]:
-                value += "'%s'" %(i)
-            else:
-                value += "'%s', " %(i)
-        value += ')'
-        self.cur.execute("INSERT INTO %s VALUES %s" %(table, value))
-        self.db.commit()
-        return True
-    
-    def select(self, query: str):
-        self.cur.execute(query)
-        result = []
-        for row in self.cur.fetchall():
-            result.append(row)
-        return result
-    
-    def delete(self, table: str, row_id: int):
-        self.cur.execute("DELETE FROM %s WHERE id=%s" %(table, str(row_id)))
-        self.db.commit()
-        return True
+    def insert(self, table: str, column: str, values: str):
+        return self.query("INSERT INTO %s (%s) VALUES (%s);" %(table, column, values))
+        
+    def delete(self, table: str, where: str):
+        return self.query(f"DELETE FROM {table} WHERE {where};")
 
     def drop(self, table: str):
-        self.cur.execute('''DROP TABLE %s''' %(table))
-        self.db.commit()
-        return True
+        return self.query(f'''DROP TABLE {table};''')
 
-    def update(self, table: str, id: int, sets: str):
-        query = '''UPDATE %s SET %s WHERE id = %s ''' %(table, sets, str(id))
-        self.cur.execute(query)
-        self.db.commit()
-        return True
+    def update(self, table: str, sets: str, where: str):
+        query = f'''UPDATE {table} SET {sets} WHERE {where};'''
+        return self.query(query)
 
     def __del__(self):
         return self.db.close()
 
 
 
-    def insert_with_id(self, table: str, values: list):
-        all = self.select('''SELECT * FROM %s''' %(table))
-        id = len(all) + 1
-        value = '(%s, ' %(id)
-        for i in values:
-            if i == values[-1]:
-                value += "'%s'" %(i)
-            else:
-                value += "'%s', " %(i)
-        value += ')'
-        self.cur.execute("INSERT INTO %s VALUES %s" %(table, value))
-        self.db.commit()
-        return True
+    def insert_into_plugins(self, plug_name: str, plug_commands: json, plug_help: json, plug_status = None, update_from_id: int = None):
+        return self.insert(
+            'plugins', 
+            'plug_name, plug_commands, plug_help, plug_status, update_from_id', 
+            f"'{plug_name}', '{plug_commands}', '{plug_help}', '{plug_status}', '{update_from_id}'"
+        )
+
+    def update_by_plug_name(self, plug_name: str, sets: str):
+        return self.update('plugins', sets, f"plug_name='{plug_name}'")
 
     def select_with_plug_name(self, plug_name: str):
-        return self.select('''SELECT * FROM plugins WHERE plug_name='%s' ''' %(plug_name))
-
-
-
+        return self.select('''SELECT * FROM plugins WHERE plug_name='%s';''' %(plug_name))
